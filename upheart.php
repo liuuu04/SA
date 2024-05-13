8b5e5d2e746debebc1f8ec1000399376c249172b
xuanyun422 <166020833+xuanyun422@users.noreply.github.com>
2024-05-13 14:37:17 +0800

    領養人上傳切結書

--- upheart.php
+++ upheart.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>愛心切結書申請</title>
-    <link rel="stylesheet" href="new2.css">
+    <link rel="stylesheet" href="test.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <style>
@@ -84,8 +84,7 @@
    $name=$_SESSION['name'];
   
    
-   $link=mysqli_connect('localhost','root','han20000914','sa');
-   mysqli_set_charset($link,"utf8");
+   $link=mysqli_connect('localhost','root','12345678','sa');
    $sql1="select * from profile where identify='$identify'";
    $result=mysqli_query($link,$sql1);
    if($row=mysqli_fetch_assoc($result))
@@ -98,7 +97,7 @@
 }
    
 
-   $link=mysqli_connect('localhost','root','han20000914','sa');
+   $link=mysqli_connect('localhost','root','12345678','sa');
    $sql="select * from pet where pet_id='$pet_id'";
    $result=mysqli_query($link,$sql);
    if($row=mysqli_fetch_assoc($result))
